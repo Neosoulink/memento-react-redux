9acebdd30ff9c286e284611ebf81865434e235e8
Neosoulink <neodarksoulink@gmail.com>
2021-05-03 16:24:53 +0100

    Comment react redux default component

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-//import React from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+// DEV
 import * as serviceWorker from './serviceWorker';
+
+// COMPONENTS
+import App from './App';
+
+// STORES
 import { store } from './app/store';
-import { ADD_TODO_ACTION } from "./features/todos/types";
+
+// STYLES
+import './index.css';
 
 
+// DEV DEBUGGING
 console.log("TODO REDUX REVISION");
-
-// DEBUGGING
 console.log(store.getState());
-store.subscribe(() => console.log(store.getState()));
 
-store.dispatch({ type: ADD_TODO_ACTION, payload: { title: "Demo dispatch" } })
-store.dispatch({ type: ADD_TODO_ACTION, payload: { title: "Demo dispatch" } })
-
-//import ReactDOM from 'react-dom';
-//import './index.css';
-//import App from './App';
-//import { Provider } from 'react-redux';
-
-//ReactDOM.render(
-//  <React.StrictMode>
-//    <Provider store={store}>
-//      <App />
-//    </Provider>
-//  </React.StrictMode>,
-//  document.getElementById('root')
-//);
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
