f78d6fa6d16e0408aadc22c070cf1e24cdad82fc
Neosoulink <neodarksoulink@gmail.com>
2021-05-03 20:45:18 +0100

    Use async action powerd by redux thunk

--- src/features/todos/actions.js
+++ src/features/todos/actions.js
@@ -1,10 +1,14 @@
+import wait from "../../helpers/wait";
 import { ADD_TODO_ACTION, DELETE_TODO_ACTION, UPDATE_FILTER_ACTION, UPDATE_TODO_ACTION } from "./types";
 
 // TODO LIST
-export const addTodoAction = (newTodo) => {
-	return { type: ADD_TODO_ACTION, payload: { title: "Added from todo list", ...newTodo } }
+export const addTodoAction = (newTodo) => async (dispatch) => {
+	await wait(2000);
+	dispatch({
+		type: ADD_TODO_ACTION,
+		payload: { title: "Added from todo list", ...newTodo }
+	})
 }
-
 export const toggleTodoAction = (todo) => {
 	return { type: UPDATE_TODO_ACTION, payload: { ...todo, completed: !todo.completed } }
 }
