e0b2aeeafa5eee9d2698b505b623ebebf862c7fe
Neosoulink <neodarksoulink@gmail.com>
2021-05-03 21:00:55 +0100

    Fix await loading

--- src/components/FormTodo.jsx
+++ src/components/FormTodo.jsx
@@ -4,22 +4,25 @@ import { addTodoAction } from "../features/todos/actions";
 
 export function FormTodo() {
 	const dispatch = useDispatch();
-	const AddTodo = React.useCallback((newTodo) => {
-		dispatch(addTodoAction(newTodo))
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
 
 	const input = React.useRef(null);
+	const [loading, setLoading] = React.useState(false);
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		AddTodo({ title: input.current.value });
+		setLoading(true);
+
+		await dispatch(addTodoAction({ title: input.current.value }))
+
+		setLoading(false);
+
 		input.current.value = null;
+		input.current.focus();
 	}
 
 
 	return <form onSubmit={handleSubmit}>
 		<input type="text" ref={input} placeholder="Tache" />
-		<button type="submit">Ajouter</button>
+		<button type="submit" disabled={loading}>{!loading ? 'Ajouter' : 'Loading...'}</button>
 	</form>
 }
