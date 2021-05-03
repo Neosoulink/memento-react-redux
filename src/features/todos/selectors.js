d151a3e87ca2fb2148f3c2889ec0bcd6ae44adc5
Neosoulink <neodarksoulink@gmail.com>
2021-05-03 19:39:21 +0100

    Use reselect to memoize filtering

--- src/features/todos/selectors.js
+++ src/features/todos/selectors.js
@@ -1,11 +1,25 @@
+import { createSelector } from "reselect";
+
 // TODO LIST
 export const todosSelector = ({ todos }) => todos;
-export const filteredTodosSelector = ({ todos, filter }) => {
-	if (filter === null) {
-		return todos
-	}
-	return todos.filter(todo => todo.completed === filter);
-}
+//export const filteredTodosSelector = ({ todos, filter }) => {
+//	if (filter === null) {
+//		return todos
+//	}
+//	return todos.filter(todo => todo.completed === filter);
+//}
 
 // TODO FILTER
 export const todosFilter = ({ filter }) => filter;
+
+export const filteredTodosSelector = createSelector(
+	todosSelector,
+	todosFilter,
+	(todos, filter) => {
+		if (filter === null) {
+			return todos;
+		}
+		return todos.filter(todo => todo.completed === filter);
+	}
+);
+
